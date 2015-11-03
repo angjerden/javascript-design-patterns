3f27fdd91e56ae3cd155838a960178572c146657
Anders Gjerde <angjerden@gmail.com>
2015-11-03 22:16:50 +0100

    Cat-clicker iteration 2 - Two cats

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,7 +1,33 @@
-var img = document.getElementById("cat");
-img.addEventListener('click', function(){
-    var counterDiv = document.getElementById("counter");
-    var number = parseInt(counterDiv.innerHTML);
-    number = number + 1;
-    counterDiv.innerHTML = number;
-}, false);
+var Cat = function(name, src) {
+    this.id = 0;
+    this.name = name;
+    this.src = src;
+    this.counter = 0;
+};
+
+var axl = new Cat('Axl', 'rs/cat.jpg');
+var bae = new Cat('Bae', 'rs/cat2.jpg');
+
+var allCats = [axl, bae];
+
+var catCounter = 1;
+allCats.forEach(function(entry) {
+    entry.id = catCounter;
+
+    //Setting name
+    var nameDiv = document.getElementById("name" + entry.id);
+    nameDiv.innerHTML = entry.name;
+
+    //Setting image
+    var img = document.getElementById("cat" + entry.id);
+    img.src = entry.src;
+
+    //Setting image click event
+    img.addEventListener('click', function(){
+        var counterDiv = document.getElementById("counter" + entry.id);
+        entry.counter++;
+        counterDiv.innerHTML = entry.counter;
+    }, false);
+
+    catCounter++;
+});
