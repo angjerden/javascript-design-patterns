c2f189254a76c3ff6b3e1cc94bcf5ce3d054da2e
Anders Gjerde <angjerden@gmail.com>
2015-11-04 23:36:46 +0100

    Cat-clicker iteration 3 - Cat-clicker premium with a lot of cats

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,33 +1,75 @@
-var Cat = function(name, src) {
-    this.id = 0;
+var CatCollection = function(array) {
+    this.array = array || [];
+    var that = this;
+    this.addCat = function(cat) {
+        this.array.push(cat);
+    };
+    this.getCatById = function(id) {
+        for(var i = 0; i < this.array.length; i++) {
+            var cat = this.array[i];
+            if (cat.id === id) {
+                return cat;
+            }
+        }
+    };
+};
+
+var Cat = function(id, name, src) {
+    this.id = id;
     this.name = name;
     this.src = src;
     this.counter = 0;
 };
 
-var axl = new Cat('Axl', 'rs/cat.jpg');
-var bae = new Cat('Bae', 'rs/cat2.jpg');
+var axl = new Cat(1, 'Axl', 'rs/cat.jpg');
+var bae = new Cat(2, 'Bae', 'rs/cat2.jpg');
+var cal = new Cat(3, 'Cal', 'rs/cat3.jpg');
+var don = new Cat(4, 'Don', 'rs/cat.jpg');
+var eri = new Cat(5, 'Eri', 'rs/cat2.jpg');
+var fry = new Cat(6, 'Fry', 'rs/cat3.jpg');
+var gob = new Cat(7, 'Gob', 'rs/cat.jpg');
+var hal = new Cat(8, 'Hal', 'rs/cat2.jpg');
+var ims = new Cat(9, 'Ims', 'rs/cat3.jpg');
+var jeq = new Cat(10, 'Jeq', 'rs/cat.jpg');
 
-var allCats = [axl, bae];
+var catCollection = new CatCollection([
+    axl, bae, cal, don, fry, gob, hal, ims, jeq
+]);
 
-var catCounter = 1;
-allCats.forEach(function(entry) {
-    entry.id = catCounter;
+var select = document.getElementById('catlist');
+var nameDiv = document.getElementById('name');
+var img = document.getElementById('image');
+var counter = document.getElementById('counter');
+
+//Adding event to select
+select.onchange = function(e) {
+    var selectedValue = parseInt(this.value);
+    var selectedCat = catCollection.getCatById(selectedValue);
 
     //Setting name
-    var nameDiv = document.getElementById("name" + entry.id);
-    nameDiv.innerHTML = entry.name;
+
+    name.innerHTML = selectedCat.name;
 
     //Setting image
-    var img = document.getElementById("cat" + entry.id);
-    img.src = entry.src;
+    img.src = selectedCat.src;
 
-    //Setting image click event
-    img.addEventListener('click', function(){
-        var counterDiv = document.getElementById("counter" + entry.id);
-        entry.counter++;
-        counterDiv.innerHTML = entry.counter;
-    }, false);
+    //Setting counter
+    counter.innerText = selectedCat.counter;
+};
 
-    catCounter++;
+catCollection.array.forEach(function(entry) {
+    //Making option for select
+    var option = document.createElement('option');
+    option.setAttribute('value', entry.id);
+    option.innerText = entry.name;
+
+    //Adding option to select
+    select.add(option);
 });
+
+//Setting image click event
+img.addEventListener('click', function(){
+    var selectedCat = catCollection.getCatById(parseInt(select.value));
+    selectedCat.counter++;
+    counter.innerHTML = selectedCat.counter;
+}, false);
